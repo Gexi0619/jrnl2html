453d6e0a5117df906ab39fe6e14fb9bc36ed30bf
Gexi0619 <sungexi@hotmail.com>
2023-05-21 03:28:17 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Jrnl to HTML
+
+Deploy your [Jrnl](https://jrnl.sh/) data as an HTML static webpage.
+
+## Deployment
+
+1. Clone the repository from GitHub
+
+```
+git clone https://github.com/Gexi0619/jrnl2html.git
+```
+
+2. Use the [following command](https://jrnl.sh/en/stable/formats/#exporting-with-file) to output the jrnl's JSON data to the root directory of this project
+
+```
+jrnl --format json --file /path/to/myjournal.json
+```
+
+3. Open `index.html` to view the webpage.
+
+   Note: When opening `index.html` locally, you may encounter CORS-related issues.
+
--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// 获取时间线容器
+// Get the timeline container
 var timelineContainer = document.getElementById("timeline");
 
-// 发起AJAX请求获取JSON数据
+// Initiate an AJAX request to get JSON data
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'test.json', true);
+xhr.open('GET', 'myjournal.json', true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4 && xhr.status === 200) {
-    // 解析JSON数据
+    // Parse the JSON data
     var jsonData = JSON.parse(xhr.responseText);
 
-    // 遍历JSON数据，创建时间线条目
+    // Iterate over the JSON data and create timeline entries
     jsonData.entries.sort(function(a, b) {
-      // 将日期和时间结合并转换为时间戳进行比较
+      // Combine the date and time, convert to timestamp for comparison
       var dateTimeA = new Date(a.date + ' ' + a.time).getTime();
       var dateTimeB = new Date(b.date + ' ' + b.time).getTime();
-      // 按时间从最新到最旧排序
+      // Sort by time from newest to oldest
       return dateTimeB - dateTimeA;
     }).forEach(function(entry) {
-      // 创建时间线条目的HTML元素
+      // Create HTML element for timeline entry
       var listItem = document.createElement("li");
       listItem.className = "timeline-item";
 
-      // 构建时间线条目内容
+      // Build the content for the timeline entry
       var content = "<h3>" + entry.title + "</h3>";
       content += "<p>" + entry.date + " " + entry.time + "</p>";
-      content += "<p>" + entry.body + "</p>"; // 添加 body 内容
+      content += "<p>" + entry.body + "</p>"; // Add the body content
 
-      // 将内容添加到时间线条目中
+      // Add the content to the timeline entry
       listItem.innerHTML = content;
 
-      // 将时间线条目添加到时间线容器中
+      // Add the timeline entry to the timeline container
       timelineContainer.appendChild(listItem);
     });
   }
